165670cd3fa50d60cc8bc5cffcfc29cd173b0726
Daniel Alexander <daniel.llumiquinga@hotmail.com>
2022-07-11 17:19:47 -0500

    [add]assets

--- src/helpers/getImgUrl.js
+++ src/helpers/getImgUrl.js
@@ -3,13 +3,13 @@ export const getImgUrl = (forecast) => {
   const isRaining = forecast.currentLocation.condition.text.includes("rain");
   if (isDay) {
     if (isRaining) {
-      return "assets/22.jpg";
+      return "src/assets/22.jpg";
     } else {
-      return "assets/24.jpg";
+      return "src/assets/24.jpg";
     }
   } else if (isRaining) {
-    return "assets/3375.jpg";
+    return "src/assets/3375.jpg";
   } else {
-    return "assets/25.jpg";
+    return "src/assets/25.jpg";
   }
 };
